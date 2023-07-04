f1c01efc552ecb39f77a50c8d3c843dc39396eb8
Bartosz Jarzyński <72377404+bartoszjarzynski@users.noreply.github.com>
2023-07-04 11:26:09 +0200

    Add files via upload

--- CropApp/CropApp/ViewController.swift
+++ CropApp/CropApp/ViewController.swift
@@ -11,14 +11,14 @@ import CoreImage
 import Photos
 import Foundation
 
-
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CropViewControllerDelegate {
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Backgroud color main view
         self.view.backgroundColor = .systemBlue
-        
+        // Title view
         let titleLabel = UILabel(frame: CGRect(x: 0, y: 100, width: view.frame.width, height: 400))
         titleLabel.textAlignment = .center
         titleLabel.textColor = .white
@@ -26,10 +26,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         titleLabel.text = "CROP PIC"
         view.addSubview(titleLabel)
         
-        
+        // Pick a photo button
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
         button.setImage(UIImage(systemName: "photo")?.withTintColor(.white, renderingMode: .alwaysOriginal), for: .normal)
-        button.setTitle("Pick a photo", for: .normal)
+        button.setTitle(" Pick a photo", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = UIColor.orange
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
@@ -38,35 +38,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         view.addSubview(button)
         
-        let label = UILabel(frame: CGRect(x: 0, y: view.frame.height - 120, width: view.frame.width, height: 100))
-        label.textAlignment = .center
-        label.textColor = .white
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.numberOfLines = 0
+        // Take a photo button
+        let cameraButton = UIButton(frame: CGRect(x: view.bounds.midX - 100, y: view.bounds.maxY - 370, width: 200, height: 50))
+        cameraButton.setImage(UIImage(systemName: "camera")?.withTintColor(.white, renderingMode: .alwaysOriginal), for: .normal)
+        cameraButton.setTitle(" Take a photo", for: .normal)
+        cameraButton.setTitleColor(.white, for: .normal)
+        cameraButton.backgroundColor = .orange
+        cameraButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        cameraButton.layer.cornerRadius = 8
+        cameraButton.addTarget(self, action: #selector(cameraTaker), for: .touchUpInside)
+        view.addSubview(cameraButton)
         
-        
-        let attributedText = NSMutableAttributedString()
-        
-        let highlighterAttachment = NSTextAttachment()
-        highlighterAttachment.image = UIImage(systemName: "highlighter")?.withRenderingMode(.alwaysTemplate)
-        highlighterAttachment.bounds = CGRect(x: 0, y: -4, width: 20, height: 20)
-        let highlighterAttributedString = NSAttributedString(attachment: highlighterAttachment)
-        attributedText.append(highlighterAttributedString)
-        
-        let createdText = NSAttributedString(string: "Created by:\n\n")
-        attributedText.append(createdText)
-        let names = ["Jakub Chrobok,", "Weronika Chruszcz,", "Bartosz Jarzyński"]
-        for name in names {
-            attributedText.append(NSAttributedString(string: " "))
-            attributedText.append(NSAttributedString(string: name))
-        }
-        attributedText.append(NSAttributedString(string: "\n"))
-        
-        label.attributedText = attributedText
-        
-        view.addSubview(label)
+        // Displaying a photo here in 300x300px
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+                imageView.contentMode = .scaleAspectFit
+                imageView.center = view.center
+                view.addSubview(imageView)
     }
     
+    // Camera roll choose
     @objc func didTapButton() {
         let picker = UIImagePickerController()
         picker.sourceType = .photoLibrary
@@ -74,20 +64,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(picker, animated: true)
     }
     
+    //Taking a picture choose
+    @objc func cameraTaker() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // Picker cancel
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true)
     }
     
+    // Show crop controller
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.originalImage] as? UIImage else {
             return
         }
         picker.dismiss(animated: true)
-        
         showCrop(image: image)
     }
     
-    
+    // Crop adjusting
     func showCrop(image: UIImage) {
         let vc = CropViewController(croppingStyle: .default, image: image)
         vc.aspectRatioPreset = .presetSquare
@@ -101,7 +100,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(vc, animated: true)
     }
     
-    
+    // Cropping view Controller
     func cropViewController(_ cropViewController: CropViewController, didFinishCancelled cancelled: Bool) {
         cropViewController.dismiss(animated: true)
     }
@@ -109,58 +108,106 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     // Declare imageView as an instance variable of your view controller class
     var imageView: UIImageView!
 
+    // Did crop controller :: after
     func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
         // May the background stay white
         self.view.backgroundColor = .white
-        
-        // Cropping
-        cropViewController.dismiss(animated: true)
-        imageView = UIImageView(frame: view.frame)
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = image
-        view.addSubview(imageView)
-        
-        // Pick once more a picture button
-        let pickbutton = UIButton(frame: CGRect(x: 20, y: view.frame.height - 100, width: 100, height: 40))
-        pickbutton.setTitle("Pick again", for: .normal)
-        pickbutton.setTitleColor(.white, for: .normal)
-        pickbutton.backgroundColor = UIColor.systemPink
-        pickbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        pickbutton.layer.cornerRadius = 8
-        pickbutton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        view.addSubview(pickbutton)
-        
-        //Monochrome scale buttom
-        let convertButton = UIButton(frame: CGRect(x: view.frame.width / 2 - 40, y: view.frame.height - 100, width: 100, height: 40))
-            convertButton.setTitle("Monoscale", for: .normal)
-            convertButton.backgroundColor = UIColor.blue
-            convertButton.setTitleColor(.white, for: .normal)
-            convertButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-            convertButton.layer.cornerRadius = 8
-            convertButton.addTarget(self, action: #selector(convertToMonochrome), for: .touchUpInside)
-            view.addSubview(convertButton)
-        
-        // Save button
-        let saveButton = UIButton(frame: CGRect(x: view.frame.width - 100, y: view.frame.height - 100, width: 80, height: 40))
-        saveButton.setTitle("Save", for: .normal)
-        saveButton.backgroundColor = UIColor.orange
-        saveButton.setTitleColor(.black, for: .normal)
-        saveButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        saveButton.layer.cornerRadius = 8
-        view.addSubview(saveButton)
-        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
-    }
 
-    @objc func saveButtonTapped() {
-        guard let imageToSave = imageView.image else {
-            // Handle the case when there is no image to save
+        // Dismiss the crop view controller
+        cropViewController.dismiss(animated: true)
+
+        // Update the existing imageView with the new cropped image
+        imageView.image = image
+
+        // Remove the previously added subviews (buttons)
+        view.subviews.forEach { subview in
+            if subview is UIButton {
+                subview.removeFromSuperview()
+            }
+        }
+
+        // Pick once more a picture button
+        let pickButton = UIButton(frame: CGRect(x: 40, y: view.frame.height - 100, width: 100, height: 40))
+        pickButton.setTitle("Pick again", for: .normal)
+        pickButton.setTitleColor(.white, for: .normal)
+        pickButton.backgroundColor = UIColor.systemPink
+        pickButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        pickButton.layer.cornerRadius = 8
+        pickButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        view.addSubview(pickButton)
+
+        // Color change button
+        let convertButton = UIButton(frame: CGRect(x: view.frame.width - 140, y: view.frame.height - 100, width: 100, height: 40))
+        convertButton.setTitle("Color", for: .normal)
+        convertButton.backgroundColor = UIColor.blue
+        convertButton.setTitleColor(.white, for: .normal)
+        convertButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        convertButton.layer.cornerRadius = 8
+        convertButton.addTarget(self, action: #selector(colorChangeTap), for: .touchUpInside)
+        view.addSubview(convertButton)
+        
+        // Share button
+        let shareButton = UIButton(frame: CGRect(x: view.frame.width - 100, y: 80, width: 80, height: 40))
+        shareButton.setTitle("Share", for: .normal)
+        shareButton.backgroundColor = UIColor.systemRed
+        shareButton.setTitleColor(.white, for: .normal)
+        shareButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        shareButton.layer.cornerRadius = 8
+        view.addSubview(shareButton)
+        shareButton.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
+    }
+    
+    // Share button options
+    @objc func shareButtonTapped() {
+        guard let imageToShare = imageView.image else {
+            // Handle the case when there is no image to share
             return
         }
-        
-        // Perform the code to save the image to the gallery
-        UIImageWriteToSavedPhotosAlbum(imageToSave, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+
+        var activityItems: [Any] = [imageToShare]
+
+        // Check if Facebook app is installed
+        if let facebookURL = URL(string: "fb://") {
+            if UIApplication.shared.canOpenURL(facebookURL) {
+                // Add Facebook activity
+                activityItems.append("Share on Facebook")
+            }
+        }
+
+        // Check if Instagram app is installed
+        if let instagramURL = URL(string: "instagram://") {
+            if UIApplication.shared.canOpenURL(instagramURL) {
+                // Add Instagram activity
+                activityItems.append("Share on Instagram")
+            }
+        }
+
+        // Check if Twitter app is installed
+        if let twitterURL = URL(string: "twitter://") {
+            if UIApplication.shared.canOpenURL(twitterURL) {
+                // Add Twitter activity
+                activityItems.append("Share on Twitter")
+            }
+        }
+
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+
+        // Exclude some activity types if desired
+        activityViewController.excludedActivityTypes = [
+            .addToReadingList,
+            .assignToContact,
+            .markupAsPDF,
+            .openInIBooks,
+            .postToTencentWeibo,
+            .postToFlickr,
+            .postToVimeo,
+            .postToWeibo
+        ]
+        present(activityViewController, animated: true, completion: nil)
     }
 
+
+    // Saving image statement
     @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error = error {
             // Handle the case when saving the image encounters an error
@@ -171,42 +218,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    // Saving in memory original image look
+    // Filter index based on original photo
     var originalImages: [UIImage: UIImage] = [:]
-
-    @objc func convertToMonochrome() {
+    var filterIndex = 0
+    
+    // Color changing function tap - sepia and BW
+    @objc func colorChangeTap() {
         guard let image = imageView.image else {
             return
         }
+        
+        let filters: [String] = ["CISepiaTone", "CIPhotoEffectMono"]
+        let filterName = filters[filterIndex]
         
         if let originalImage = originalImages[image] {
             // Revert to the original color image
             imageView.image = originalImage
             originalImages.removeValue(forKey: image)
         } else {
-            // Convert to monochrome
-            guard let ciImage = CIImage(image: image) else {
-                return
+            // Apply the selected filter
+            let filter = CIFilter(name: filterName)!
+            filter.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+            
+            if let outputImage = filter.outputImage {
+                let context = CIContext(options: nil)
+                guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+                    return
+                }
+                
+                // Create a new UIImage with the filter applied
+                let filteredImage = UIImage(cgImage: cgImage)
+                
+                // Store both the original color image and the filtered image
+                originalImages[image] = image
+                originalImages[filteredImage] = image
+                
+                // Update the imageView with the filtered image
+                imageView.image = filteredImage
             }
-            
-            let context = CIContext(options: nil)
-            let filter = CIFilter(name: "CIColorControls")!
-            filter.setValue(ciImage, forKey: kCIInputImageKey)
-            filter.setValue(0.0, forKey: kCIInputSaturationKey)
-            
-            guard let outputImage = filter.outputImage,
-                  let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
-                return
-            }
-            
-            let monochromeImage = UIImage(cgImage: cgImage)
-            
-            // Store both the original color image and the monochrome image
-            originalImages[image] = image
-            originalImages[monochromeImage] = image
-            
-            // Update the imageView with the monochrome image
-            imageView.image = monochromeImage
         }
+        
+        // Update the filter index for the next click
+        filterIndex = (filterIndex + 1) % filters.count
     }
-
+    
 }
+
